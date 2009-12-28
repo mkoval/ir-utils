ce12b2e09e13c367f188cbb524d586aacf445f1c
Owen Healy <ellbur@gmail.com>
2009-12-27 23:11:01 -0500

    18 millisecond script

---
+++ slow.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+from __future__ import print_function
+import time
+import sys
+
+for line in sys.stdin:
+	print(line, end='')
+	sys.stdout.flush()
+	
+	time.sleep(18.6 / 1000)
+
